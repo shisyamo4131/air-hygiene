f17ea1570ccd49e644509dcb4f5b57d53d6e4dea
shisyamo4131 <sevenstar_1226@hotmail.com>
2023-10-25 16:36:52 +0900

    commit

--- models/Site.js
+++ models/Site.js
@@ -10,7 +10,7 @@ export default class Site extends FireModel {
     this.hasMany = [
       {
         collection: 'CollectionResults',
-        field: 'siteId',
+        field: 'site.docId',
         condition: '==',
         type: 'collection',
       },
